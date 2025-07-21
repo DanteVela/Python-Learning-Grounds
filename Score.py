0e510a5f53df3e1b1cd7cc44c8752fd6f84c7ed1
DanteVela <70284605+DanteVela@users.noreply.github.com>
2025-07-21 13:03:31 -0500

    Revised Score File

--- Score.py
+++ Score.py
@@ -3,10 +3,14 @@
 class Score:
     def __init__(self, points):
         self.points = points
-
+    
+    # Dunder repr method to provide a string representation of the Score instance.
+    # This is useful for debugging and logging purposes.
     def __repr__(self):
         return f"Score(points={self.points})"
-
+    
+    # Dunder add method to allow addition of Score instances.
+    # This enables the use of the '+' operator to combine scores.
     def __add__(self, other):
         return Score(self.points + other.points)
 
